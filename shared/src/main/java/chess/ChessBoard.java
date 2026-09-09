f0b522550c7f7bfe29ff5e20488b750f24b7e75e
Roverthv <roverthv@byu.edu>
2026-09-09 11:50:52 -0600

    Implemented pieceColor and type, along with respective gets.

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -8,7 +8,7 @@ package chess;
  */
 public class ChessBoard {
 
-    private ChessPiece[][] board = new ChessPiece[8][8];
+    private final ChessPiece[][] board = new ChessPiece[8][8];
 
     public ChessBoard() {
         
@@ -21,7 +21,6 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        //throw new RuntimeException("Not implemented");
         board[position.getRow()-1][position.getColumn()-1] = piece;
     }
 
@@ -33,7 +32,6 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        //throw new RuntimeException("Not implemented");
         return board[position.getRow()-1][position.getColumn()-1];
     }
 
--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -8,8 +8,8 @@ package chess;
  */
 public class ChessPosition {
 
-    private int row;
-    private int col;// figure out row and column
+    private final int row;
+    private final int col;// figure out row and column
     public ChessPosition(int row, int col) {
         this.row = row;
         this.col = col;
@@ -20,7 +20,6 @@ public class ChessPosition {
      * 1 codes for the bottom row
      */
     public int getRow() {
-        //throw new RuntimeException("Not implemented");
         return row;
     }
 
@@ -29,7 +28,6 @@ public class ChessPosition {
      * 1 codes for the left column
      */
     public int getColumn() {
-        //throw new RuntimeException("Not implemented");
         return col;
     }
 }
