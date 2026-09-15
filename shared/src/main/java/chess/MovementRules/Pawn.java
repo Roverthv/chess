fd7f0321d152b48378e804edb42df11f84b2bfab
Roverthv <roverthv@byu.edu>
2026-09-15 10:22:25 -0600

    Changed Pawn findMoves to handle being blocked from double jump.

--- shared/src/main/java/chess/MovementRules/Pawn.java
+++ shared/src/main/java/chess/MovementRules/Pawn.java
@@ -10,40 +10,57 @@ import java.util.List;
 
 public class Pawn implements PiecePatterns {
 
-    private static final int[][][] movementPatternsWhite = {{{2,0}},{{1,0}},{{1,1},{1,-1}}};
-    private static final int[][][] movementPatternsBlack = {{{-2,0}},{{-1,0}},{{-1,-1},{-1,1}}};
+    private static final int[][][] movementPatternsWhite = {{{1,0}, {2,0}},{{1,1},{1,-1}}};
+    private static final int[][][] movementPatternsBlack = {{{-1,0}, {-2,0}},{{-1,-1},{-1,1}}};
     public Pawn() {}
 
     private static List<ChessMove> evaluateMoves(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor pieceColor, Boolean hasMoved, int[][][] movementPatterns){
         List<ChessMove> moves = new ArrayList<>();
-        int[] offset;
         int startRow = 7;
         int promoteRow = 1;
         if (pieceColor == ChessGame.TeamColor.WHITE){
             startRow = 2;
             promoteRow = 8;
         }
-        if (!hasMoved && myPosition.getRow() == startRow){
-            offset = movementPatterns[0][0];
+//        if (!hasMoved && myPosition.getRow() == startRow){
+//            offset = movementPatterns[0][0];
+//            ChessPosition target = myPosition.addOffset(offset[0], offset[1]);
+//            var valid = board.isValidMove(target, pieceColor);
+//            if (valid[0] && !valid[1]) {
+//                moves.add(new ChessMove(myPosition, target, null));
+//            }
+//        }
+//        offset = movementPatterns[1][0];
+//        ChessPosition target = myPosition.addOffset(offset[0], offset[1]);
+//        var valid = board.isValidMove(target, pieceColor);
+//        if (valid[0] && !valid[1]) {
+//            if (target.getRow() == promoteRow){
+//                // figure out how to have them select new piece type
+//                moves.add(new ChessMove(myPosition, target, null));
+//            }
+//            moves.add(new ChessMove(myPosition, target, null));
+//        }
+        for (int i = 0; i < 2; i++){
+            int[] offset = movementPatterns[0][i];
+            if (i == 1 && (hasMoved || myPosition.getRow() != startRow)){
+                break;
+            }
             ChessPosition target = myPosition.addOffset(offset[0], offset[1]);
             var valid = board.isValidMove(target, pieceColor);
             if (valid[0] && !valid[1]) {
+                if (target.getRow() == promoteRow){
+                    // figure out how to have them select new piece type
+                    moves.add(new ChessMove(myPosition, target, null));
+                }
                 moves.add(new ChessMove(myPosition, target, null));
             }
-        }
-        offset = movementPatterns[1][0];
-        ChessPosition target = myPosition.addOffset(offset[0], offset[1]);
-        var valid = board.isValidMove(target, pieceColor);
-        if (valid[0] && !valid[1]) {
-            if (target.getRow() == promoteRow){
-                // figure out how to have them select new piece type
-                moves.add(new ChessMove(myPosition, target, null));
+            else {
+                break;
             }
-            moves.add(new ChessMove(myPosition, target, null));
         }
-        for (int[] offsets : movementPatterns[2]) {
-            target = myPosition.addOffset(offsets[0], offsets[1]);
-            valid = board.isValidMove(target, pieceColor);
+        for (int[] offsets : movementPatterns[1]) {
+            ChessPosition target = myPosition.addOffset(offsets[0], offsets[1]);
+            var valid = board.isValidMove(target, pieceColor);
             if(valid[0] && valid[1]){
                 if (target.getRow() == promoteRow){
                     // figure out how to have them select new piece type
